ef22083e1aa1a831a2c5255a884c0af5b07965cf
ruffsl <roxfoxpox@gmail.com>
2021-05-21 23:21:59 -0700

    Add scaffolding

---
+++ colcon_clean/__init__.py
@@ -0,0 +1,4 @@
+# Copyright 2021 Ruffin White
+# Licensed under the Apache License, Version 2.0
+
+__version__ = '0.0.0'
---
+++ setup.py
@@ -0,0 +1,6 @@
+# Copyright 2021 Ruffin White
+# Licensed under the Apache License, Version 2.0
+
+from setuptools import setup
+
+setup()
