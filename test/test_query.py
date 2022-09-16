d0202fb26cb7c7d467cc03955c4e8fcabc48aba5
ruffsl <roxfoxpox@gmail.com>
2022-09-16 17:26:17 +0200

    Add outline of query test

---
+++ test/test_query.py
@@ -0,0 +1,27 @@
+# Copyright 2021 Ruffin White
+# Licensed under the Apache License, Version 2.0
+
+# from colcon_clean.clean.query import query_yes_no
+# from mock import Mock
+
+
+# class Object(object):
+#     pass
+
+
+def test_query_interface():
+    pass
+    # interface = CleanVerb()
+    # interface._subparser = Object()
+    # interface._subparser.format_usage = Mock(return_value='')
+
+    # args = Object()
+    # context = CommandContext(command_name='clean', args=args)
+
+    # context.args.subverb_name = None
+    # rc = interface.main(context=context)
+    # assert rc == 'Error: No subverb provided'
+
+    # context.args.subverb_name = 'lock'
+    # rc = interface.main(context=context)
+    # assert rc is None
