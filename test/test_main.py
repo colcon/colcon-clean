120b1777739a9b0123118a674974832d8188902e
ruffsl <roxfoxpox@gmail.com>
2022-09-17 17:19:36 +0200

    Extend main tests

--- test/test_main.py
+++ test/test_main.py
@@ -22,6 +22,8 @@ def test_main(monkeypatch):
 
     try:
         main(argv=argv + ['build'])
+
+        # Clean all package base paths explicitly
         main(argv=argv + ['clean', 'packages', '--yes', \
             '--base-select', \
                 'build', \
@@ -32,12 +34,24 @@ def test_main(monkeypatch):
                 'test-package-b', \
                 'test-package-c'])  # noqa
 
+        # Clean workspace build base paths of python files
+        # And with duplicate match filters
         main(argv=argv + ['clean', 'workspace', '--yes', \
             '--base-select', \
                 'build', \
             '--clean-match', \
+                '*.py', \
                 '*.py'])  # noqa
 
+        # Try again but with nothing left to clean
+        main(argv=argv + ['clean', 'workspace', '--yes', \
+            '--base-select', \
+                'build', \
+            '--clean-match', \
+                '*.py', \
+                '*.py'])  # noqa
+
+        # Clean all workspace base paths explicitly
         main(argv=argv + ['clean', 'workspace', '--yes', \
             '--base-select', \
                 'build', \
@@ -45,12 +59,24 @@ def test_main(monkeypatch):
                 'log', \
                 'test_result'])  # noqa
 
+        # Try again but with nothing left to clean
         main(argv=argv + ['clean', 'workspace', '--yes'])  # noqa
 
         main(argv=argv + ['build'])
 
+        # Clean workspace base paths when prompted by user input
         monkeypatch.setattr('builtins.input', lambda _: 'y')
         main(argv=argv + ['clean', 'workspace'])  # noqa
+
+        # Try cleaning packages with invalid base handler selection
+        main(argv=argv + ['clean', 'packages', '--yes', \
+            '--base-select', \
+                'foo'])  # noqa
+
+        # Try cleaning workspace with invalid base handler selection
+        main(argv=argv + ['clean', 'workspace', '--yes', \
+            '--base-select', \
+                'foo'])  # noqa
 
         print('ws_base: ', ws_base)
     finally:
@@ -58,8 +84,3 @@ def test_main(monkeypatch):
         # therefore only try to delete the temporary directory
         # shutil.rmtree(ws_base, ignore_errors=True)
         pass
-
-    # check files exist
-    # shutil.rmtree(ws_base / 'build')
-    # shutil.rmtree(ws_base / 'install')
-    # # shutil.rmtree(ws_base / 'log')
