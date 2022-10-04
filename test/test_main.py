colcon/colcon-clean

commit 28804a48578f14565260004e7ec60e350fb85a3c

Simplify if confirmed

colcon_clean/subverb/__init__.py
@@ -229,17 +229,16 @@ def clean_paths(paths, confirmed=False):
         return
 
     cwd_path = Path.cwd()
-    if not confirmed:
+    if confirmed:
+        for path in paths:
+            _clean_path(path)
+    else:
         print('Paths:')
         for path in sorted(paths):
             path = path.relative_to(cwd_path)
             print('    ', path)
         question = 'Clean the above paths?'
         confirmed = query_yes_no(question)
-
-    if confirmed:
-        for path in paths:
-            _clean_path(path)
 
 
 def _onerror(func, path, excinfo):  # pragma: no cover

commit dab9c4b914df11148faf1c85f356039ac1df3c22

Fix confirm regression (#28)
* Add colcon-defaults to test extras_require
to set default test-result-base base path

* Expand tests to check filesystem effects

* Fix deprecation warning for logger
DeprecationWarning: The 'warn' method is deprecated, use 'warning' instead

* Revert regression with confirmed
from commit:
https://github.com/colcon/colcon-clean/pull/25/commits/28804a48578f14565260004e7ec60e350fb85a3c

* Fix comment typo

colcon_clean/subverb/__init__.py
@@ -229,10 +229,7 @@ def clean_paths(paths, confirmed=False):
         return
 
     cwd_path = Path.cwd()
-    if confirmed:
-        for path in paths:
-            _clean_path(path)
-    else:
+    if not confirmed:
         print('Paths:')
         for path in sorted(paths):
             path = path.relative_to(cwd_path)
@@ -240,10 +237,14 @@ def clean_paths(paths, confirmed=False):
         question = 'Clean the above paths?'
         confirmed = query_yes_no(question)
 
+    if confirmed:
+        for path in paths:
+            _clean_path(path)
+
 
 def _onerror(func, path, excinfo):  # pragma: no cover
     if excinfo[0] in (OSError, PermissionError):  # pragma: no branch
-        logger.warn(
+        logger.warning(
             "Skipping path: '{path}'".format_map(locals()))
         logger.info(
             "Skipping info: '{excinfo[1]}'".format_map(locals()))

setup.cfg
@@ -34,6 +34,7 @@ zip_safe = true
 
 [options.extras_require]
 test =
+  colcon-defaults
   colcon-package-information>=0.3.3
   colcon-package-selection>=0.2.10
   colcon-recursive-crawl
@@ -60,6 +61,7 @@ exclude = test
 [tool:pytest]
 filterwarnings =
     error
+    ignore::DeprecationWarning:colcon_defaults:
     ignore::DeprecationWarning:flake8:
     ignore::DeprecationWarning:scantree.*:
     ignore::PendingDeprecationWarning:scspell:

test/resources/defaults.yaml
@@ -1,0 +1,13 @@
+_common: &common
+  "test-result-base": "test_results"
+
+"clean.packages":
+  <<: *common
+"clean.workspace":
+  <<: *common
+"build":
+  <<: *common
+"test":
+  <<: *common
+"test-result":
+  <<: *common

test/spell_check.words
@@ -34,3 +34,4 @@ tempfile
 thomas
 wildcard
 workspaces
+yaml

test/test_main.py
@@ -20,6 +20,7 @@ argparse.ArgumentParser.error = _raising_error
 
 
 def test_main(monkeypatch):
+    """System test for colcon clean CLI."""
     ws_base = Path(mkdtemp(prefix='test_colcon_'))
     resources_base = Path('test', 'resources').absolute()
     shutil.copytree(resources_base / 'test_src', ws_base / 'src')
@@ -29,9 +30,12 @@ def test_main(monkeypatch):
     os.environ['COLCON_EXTENSION_BLOCKLIST'] = (
         'colcon_core.event_handler.desktop_notification:' +
         os.environ.get('COLCON_EXTENSION_BLOCKLIST', ''))
+    os.environ['COLCON_DEFAULTS_FILE'] = \
+        (resources_base / 'defaults.yaml').as_posix()
 
     try:
         main(argv=argv + ['build'])
+        main(argv=argv + ['test'])
 
         # Clean all package base paths explicitly
         main(argv=argv + ['clean', 'packages', '--yes', \
@@ -44,6 +48,22 @@ def test_main(monkeypatch):
                 'test-package-b', \
                 'test-package-c'])  # noqa
 
+        # Assert unselected packages are skipped
+        assert (ws_base / 'build' / 'test-package-a').exists()
+        assert (ws_base / 'install' / 'test-package-a').exists()
+        assert (ws_base / 'test_results' / 'test-package-a').exists()
+
+        # Assert selected packages are cleaned
+        assert not (ws_base / 'build' / 'test-package-b').exists()
+        assert not (ws_base / 'build' / 'test-package-c').exists()
+        assert not (ws_base / 'install' / 'test-package-b').exists()
+        assert not (ws_base / 'install' / 'test-package-c').exists()
+        assert not (ws_base / 'test_results' / 'test-package-b').exists()
+        assert not (ws_base / 'test_results' / 'test-package-c').exists()
+
+        # Assert inapplicable base paths are skipped
+        assert (ws_base / 'log').exists()
+
         # Clean workspace build base paths of python files
         # And with duplicate match filters
         main(argv=argv + ['clean', 'workspace', '--yes', \
@@ -52,6 +72,14 @@ def test_main(monkeypatch):
             '--clean-match', \
                 '*.py', \
                 '*.py'])  # noqa
+
+        # Assert workspace matches are cleaned
+        assert not (ws_base / 'build' / 'test-package-a' / 'build' / 'lib' /
+                    'test_package_a ' / '__init__.py').exists()
+
+        # Assert workspace misses are not cleaned
+        assert (ws_base / 'build' / 'test-package-a' /
+                'colcon_build.rc').exists()
 
         # Try again but with nothing left to clean
         main(argv=argv + ['clean', 'workspace', '--yes', \
@@ -69,10 +97,18 @@ def test_main(monkeypatch):
                 'log', \
                 'test_result'])  # noqa
 
-        # Try again but with nothing left to clean
+        # Assert workspace base paths are cleaned
+        assert not (ws_base / 'build').exists()
+        assert not (ws_base / 'install').exists()
+        assert not (ws_base / 'test_results').exists()
+        # Exception for log, as clean verb generates its own
+        # assert not (ws_base / 'log').exists()
+
+        # Try again implicitly but with nothing left to clean
         main(argv=argv + ['clean', 'workspace', '--yes'])  # noqa
 
         main(argv=argv + ['build'])
+        main(argv=argv + ['test'])
 
         # Don't clean workspace base paths when prompted by user input
         monkeypatch.setattr('builtins.input', lambda: 'n')
@@ -83,13 +119,61 @@ def test_main(monkeypatch):
             '--base-ignore', \
                 'log'])  # noqa
 
-        # Ignore one workspace base paths explicitly
+        # Assert no workspace base paths are cleaned
+        assert (ws_base / 'build').exists()
+        assert (ws_base / 'install').exists()
+        assert (ws_base / 'test_results').exists()
+        assert (ws_base / 'log').exists()
+
+        # Ignore one package base paths explicitly
         main(argv=argv + ['clean', 'packages', \
             '--base-ignore', \
                 'log'])  # noqa
 
+        # Assert no package base paths are cleaned
+        assert (ws_base / 'build' / 'test-package-a').exists()
+        assert (ws_base / 'build' / 'test-package-b').exists()
+        assert (ws_base / 'build' / 'test-package-c').exists()
+        assert (ws_base / 'install' / 'test-package-a').exists()
+        assert (ws_base / 'install' / 'test-package-b').exists()
+        assert (ws_base / 'install' / 'test-package-c').exists()
+        assert (ws_base / 'test_results' / 'test-package-a').exists()
+        assert (ws_base / 'test_results' / 'test-package-b').exists()
+        assert (ws_base / 'test_results' / 'test-package-c').exists()
+        assert (ws_base / 'log').exists()
+
         # Clean workspace base paths when prompted by user input
         monkeypatch.setattr('builtins.input', lambda: 'y')
+
+        # Ignore one package explicitly
+        main(argv=argv + ['clean', 'packages', \
+            '--packages-skip', \
+                'test-package-a'])  # noqa
+
+        # Assert unselected packages are skipped
+        assert (ws_base / 'build' / 'test-package-a').exists()
+        assert (ws_base / 'install' / 'test-package-a').exists()
+        assert (ws_base / 'test_results' / 'test-package-a').exists()
+
+        # Assert selected packages are cleaned
+        assert not (ws_base / 'build' / 'test-package-b').exists()
+        assert not (ws_base / 'build' / 'test-package-c').exists()
+        assert not (ws_base / 'install' / 'test-package-b').exists()
+        assert not (ws_base / 'install' / 'test-package-c').exists()
+        assert not (ws_base / 'test_results' / 'test-package-b').exists()
+        assert not (ws_base / 'test_results' / 'test-package-c').exists()
+
+        # Ignore one workspace base paths explicitly
+        main(argv=argv + ['clean', 'workspace', \
+            '--base-ignore', \
+                'log'])  # noqa
+
+        # Assert implicitly selected workspace base paths are cleaned
+        assert not (ws_base / 'build').exists()
+        assert not (ws_base / 'install').exists()
+        assert not (ws_base / 'test_results').exists()
+        assert (ws_base / 'log').exists()
+
         main(argv=argv + ['clean', 'workspace'])  # noqa
 
         with pytest.raises(argparse.ArgumentError):