colcon/colcon-clean

commit 35660e24988d7a94ed9ecc98d67c194d0514ec73

Fix deprecations (#40)
* Migrate from deprecated  rmtree arg
onerror is now deprecated for onexc

* Use f-strings

colcon_clean/subverb/__init__.py
@@ -242,20 +242,17 @@ def clean_paths(paths, confirmed=False):
             _clean_path(path)
 
 
-def _onerror(func, path, excinfo):  # pragma: no cover
-    if excinfo[0] in (OSError, PermissionError):  # pragma: no branch
-        logger.warning(
-            "Skipping path: '{path}'".format_map(locals()))
-        logger.info(
-            "Skipping info: '{excinfo[1]}'".format_map(locals()))
+def _onexc(func, path, excinfo):  # pragma: no cover
+    if isinstance(excinfo, (PermissionError, OSError)):  # pragma: no branch
+        logger.warning(f"Skipping path: '{path}'")
+        logger.info(f"Skipping info: '{excinfo}'")
         return
     raise
 
 
 def _clean_path(path):
-    logger.info(
-        "Cleaning path: '{path}'".format_map(locals()))
+    logger.info(f"Cleaning path: '{path}'")
     if path.is_dir():
-        shutil.rmtree(path, onerror=_onerror)
+        shutil.rmtree(path, onexc=_onexc)
     else:
         path.unlink()

test/spell_check.words
@@ -19,6 +19,7 @@ mkdtemp
 monkeypatch
 nargs
 noqa
+onexc
 pathlib
 plugin
 pydocstyle

colcon_clean/base_handler/__init__.py
@@ -74,7 +74,7 @@ def add_base_handler_arguments(parser):
         choices=extension_keys,
         default=extension_keys,
         help='Select base names to clean in workspace '
-             '(default: {extension_keys})'.format_map(locals()))
+             f'(default: {extension_keys})')
 
     group.add_argument(
         '--base-ignore', nargs='*', metavar='BASE_NAME',

colcon_clean/base_handler/build.py
@@ -22,7 +22,7 @@ class BuildBaseHandler(BaseHandlerExtensionPoint):
             '--build-base',
             default=self.base_path,
             help='The base path for all build directories '
-                 '(default: {self.base_path})'.format_map(locals()))
+                 f'(default: {self.base_path})')
 
     def get_workspace_paths(self, *, args):  # noqa: D102
         return [args.build_base]

colcon_clean/base_handler/install.py
@@ -22,7 +22,7 @@ class InstallBaseHandler(BaseHandlerExtensionPoint):
             '--install-base',
             default=self.base_path,
             help='The base path for all install directories '
-                 '(default: {self.base_path})'.format_map(locals()))
+                 f'(default: {self.base_path})')
 
     def get_workspace_paths(self, *, args):  # noqa: D102
         return [args.install_base]

colcon_clean/base_handler/log.py
@@ -20,7 +20,7 @@ class LogBaseHandler(BaseHandlerExtensionPoint):
             '--log-base',
             default=self.base_path,
             help='The base path for all log directories '
-                 '(default: {self.base_path})'.format_map(locals()))
+                 f'(default: {self.base_path})')
 
     def get_workspace_paths(self, *, args):  # noqa: D102
         return [args.log_base]

colcon_clean/base_handler/test_result.py
@@ -24,7 +24,7 @@ class TestResultBaseHandler(BaseHandlerExtensionPoint):
             '--test-result-base',
             default=self.base_path,
             help='The base path for all test_result directories '
-                 '(default: {self.base_path})'.format_map(locals()))
+                 f'(default: {self.base_path})')
 
     def get_workspace_paths(self, *, args):  # noqa: D102
         return [args.test_result_base]

colcon_clean/subverb/packages.py
@@ -48,9 +48,7 @@ class PackagesCleanSubverb(CleanSubverbExtensionPoint):
 
         for base_name in args.base_select:
             if base_name in args.base_ignore:
-                logger.info(
-                    "Ignoring base handler for selection '{base_name}'"
-                    .format_map(locals()))
+                logger.info(f"Ignoring base handler for selection '{base_name}'")
                 continue
             base_handler_extension = base_handler_extensions[base_name]
             for decorator in decorators:

commit a1b598049f56100d6264e37034fd7dd3bba32e01

Restore support for Python < 3.12
Fixes: 35660e2

colcon_clean/subverb/__init__.py
@@ -243,6 +243,14 @@ def clean_paths(paths, confirmed=False):
             _clean_path(path)
 
 
+def _onerror(func, path, excinfo):  # pragma: no cover
+    if excinfo[0] in (OSError, PermissionError):  # pragma: no branch
+        logger.warning(f"Skipping path: '{path}'")
+        logger.info(f"Skipping info: '{excinfo[1]}'")
+        return
+    raise
+
+
 def _onexc(func, path, excinfo):  # pragma: no cover
     if isinstance(excinfo, (PermissionError, OSError)):  # pragma: no branch
         logger.warning(f"Skipping path: '{path}'")
@@ -254,6 +262,10 @@ def _onexc(func, path, excinfo):  # pragma: no cover
 def _clean_path(path):
     logger.info(f"Cleaning path: '{path}'")
     if path.is_dir():
-        shutil.rmtree(path, onexc=_onexc)
+        try:
+            shutil.rmtree(path, onexc=_onexc)
+        except TypeError:
+            # TODO: Remove when minimum python version is 3.12
+            shutil.rmtree(path, onerror=_onerror)
     else:
         path.unlink()

test/spell_check.words
@@ -37,6 +37,7 @@ subparsers
 subverb
 tempfile
 thomas
+todo
 unittest
 wildcard
 workspaces