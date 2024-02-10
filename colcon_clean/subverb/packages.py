1b759ab01eb0a493b12c4d73cbbe905b71e17320
Scott K Logan <logans@cottsay.net>
2024-02-09 18:11:31 -0600

    Address a couple of flake8 regressions (#44)

--- colcon_clean/subverb/packages.py
+++ colcon_clean/subverb/packages.py
@@ -48,7 +48,8 @@ class PackagesCleanSubverb(CleanSubverbExtensionPoint):
 
         for base_name in args.base_select:
             if base_name in args.base_ignore:
-                logger.info(f"Ignoring base handler for selection '{base_name}'")
+                logger.info(
+                    f"Ignoring base handler for selection '{base_name}'")
                 continue
             base_handler_extension = base_handler_extensions[base_name]
             for decorator in decorators:
